be067baa808abf55ca51ea1e8a565fcf42f39368
sanidh-id <sanidh.id@polussolutions.com>
2024-06-27 10:56:59 +0530

    Added Ids to main, moved current user function in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,19 @@
 window.onload = function checkLogin() {
-    // console.log(localStorage.getItem("status"));
+  // console.log(localStorage.getItem("status"));
 
-    if (localStorage.getItem("status") === "true") {
-      // alert("Success");
-      // return true;
-    } else {
-      alert("Please login 🙏");
-      location.href = "login.html";
-    }
-  }
-
-  function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.substring(1);
+  if (localStorage.getItem("status") === "true") {
+    // alert("Success");
+    // return true;
+  } else {
+    alert("Please login 🙏");
+    location.href = "login.html";
   }
 
   let user = localStorage.getItem("current-user");
   document.getElementById("current-user").innerHTML =
     "Hi " + capitalize(user) + "!";
+};
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.substring(1);
+}
