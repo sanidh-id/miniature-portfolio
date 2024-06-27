2b1d48c581bee298852c4602d987fb5dba0c1512
sanidh-id <sanidh.id@polussolutions.com>
2024-06-27 10:58:52 +0530

    Changed contact.js

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,10 +1,27 @@
 window.onload = function checkLogin() {
-    // console.log(localStorage.getItem("status"));
-    if (localStorage.getItem("status") === "true") {
-      // alert("Success");
-      // return true;
-    } else {
-      alert("Please login 🙏");
-      location.href = "login.html";
-    }
+  // console.log(localStorage.getItem("status"));
+  if (localStorage.getItem("status") === "true") {
+    // alert("Success");
+    // return true;
+  } else {
+    alert("Please login 🙏");
+    location.href = "login.html";
   }
+};
+
+function submitForm() {
+  // Retrieve input values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Construct message to display
+  var alertMessage = "Name: " + name + "\n";
+  alertMessage += "Email: " + email + "\n";
+  alertMessage += "Message: " + message;
+
+  // Display message in alert
+  alert(alertMessage);
+
+  document.getElementById("myForm").reset();
+}
